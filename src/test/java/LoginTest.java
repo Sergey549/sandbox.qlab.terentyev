4fcfd6dcb2c164859795581da2e745748c29337d
Sergey549 <55460819+Sergey549@users.noreply.github.com>
2020-04-28 20:57:33 +0300

    Создание методов для заполнения полей Имени пользователя и пароля

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -11,13 +11,15 @@ public class LoginTest extends TestBase {
     }
 
     private void incorrectAuth(String username, String password) {
-        driver.findElement(By.name("_username")).click();
-        driver.findElement(By.name("_username")).clear();
-        driver.findElement(By.name("_username")).sendKeys(username);
-        driver.findElement(By.id("password")).click();
-        driver.findElement(By.id("password")).clear();
-        driver.findElement(By.id("password")).sendKeys(password);
+        fieldFormFilling(By.name("_username"), username);
+        fieldFormFilling(By.id("password"), password);
         driver.findElement(By.cssSelector("input[value='Войти']")).click();
+    }
+
+    private void fieldFormFilling(By locator, String text) {
+        driver.findElement(locator).click();
+        driver.findElement(locator).clear();
+        driver.findElement(locator).sendKeys(text);
     }
 
 }
