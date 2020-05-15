9181a37f6bdac4b01548279739beeb8e94e8f696
Sergey549 <55460819+Sergey549@users.noreply.github.com>
2020-05-15 19:06:22 +0300

    PageObject structure

--- src/test/java/login/LoginNegativeTests.java
+++ src/test/java/login/LoginNegativeTests.java
@@ -2,24 +2,102 @@ package login;
 
 import org.junit.Assert;
 import org.junit.Test;
-
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class LoginNegativeTests extends TestBase {
-
 
     @Test
     public void incorrectUserNameAndPassword() {
 
-        loginPage.icorrectLogin("TestUser", "Password");
+        String userName = "TestUser";
+        String userPassword = "Password";
+
+        new ModernLoginPage()
+                .openLoginPage()
+                .fillUserNameField(userName)
+                .fillUserPasswordField(userPassword)
+                .catchAnExceptionMethod()
+                .enterButtonClick()
+                .checkValidationWebElementIsPresent();
     }
 
     @Test
-    public void urlCheck () {
 
-        loginPage.enterButtonClick();
-        loginPage.catchAnException();
-        String url = loginPage.driver.getCurrentUrl();
-        Assert.assertEquals(url, "https://tt-develop.quality-lab.ru/login" );
+    public void checkUrl() {
 
+        new ModernLoginPage()
+                .openLoginPage()
+                .enterButtonClick()
+                .catchAnExceptionMethod()
+                .checkCurrentUrlIsLoginPage();
     }
+
+
+
+    
+    public class ModernLoginPage {
+
+        @FindBy(name = "_username")
+        private WebElement userField;
+
+        @FindBy(id = "password")
+        private WebElement passwordField;
+
+        @FindBy(css = "input[value='Войти']")
+        private WebElement enterButton;
+
+        @FindBy(xpath = ".//*[text()='Invalid credentials.']/..")
+        private WebElement hiddenValidationWebElement;
+
+
+        public ModernLoginPage openLoginPage() {
+            driver.get("https://tt-develop.quality-lab.ru/login");
+            PageFactory.initElements(driver, this);
+            return this;
+        }
+
+        public ModernLoginPage fillUserNameField(String userName) {
+            userField.click();
+            userField.clear();
+            userField.sendKeys(userName);
+            return this;
+        }
+
+        public ModernLoginPage fillUserPasswordField(String userPassword) {
+            passwordField.click();
+            passwordField.clear();
+            passwordField.sendKeys(userPassword);
+            return this;
+        }
+
+        public ModernLoginPage catchAnExceptionMethod() {
+            try {
+                checkValidationWebElementIsPresent();
+            } catch (NoSuchElementException ignored) {
+                System.out.println("The entered userdata is correct. You will be logged in");
+            }
+            return this;
+        }
+
+        public ModernLoginPage enterButtonClick() {
+            enterButton.click();
+            return this;
+        }
+
+        public ModernLoginPage checkValidationWebElementIsPresent() {
+            hiddenValidationWebElement.isDisplayed();
+            return this;
+        }
+
+        public ModernLoginPage checkCurrentUrlIsLoginPage() {
+            driver.getCurrentUrl();
+            String URL = driver.getCurrentUrl();
+            Assert.assertEquals(URL, "https://tt-develop.quality-lab.ru/login");
+            return this;
+        }
+    }
+
 }
--- src/test/java/login/TestBase.java
+++ src/test/java/login/TestBase.java
@@ -5,46 +5,38 @@ import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.util.concurrent.TimeUnit;
 
+public class TestBase  {
+    public WebDriver driver;
 
-public abstract class TestBase {
-
-    protected final LoginPage loginPage = new LoginPage();
-    public WebDriver wait;
 
     @Before
     public void initWebDriver() {
-
         iniWebDriver();
     }
 
     public void iniWebDriver() {
         ChromeOptions options = new ChromeOptions();
-        loginPage.driver = new ChromeDriver(options);
+        driver = new ChromeDriver(options);
         //Запуск браузера в окне размером 500х500
         //options.addArguments("--window-size=500,500");
         //Запуск браузера на весь экран
-        loginPage.driver.manage().window().maximize();
-        loginPage.driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         //Меняем заданный при инициализации размер окна
         //driver.manage().window().setSize(new Dimension(210, 100));
-        loginPage.driver.get("https://tt-develop.quality-lab.ru/login");
-        WebDriverWait wait = new WebDriverWait(loginPage.driver, 5);
+        driver.get("https://tt-develop.quality-lab.ru/login");
+        WebDriverWait wait = new WebDriverWait(driver, 5);
     }
 
     @After
     public void quitBrowser() {
-        if (loginPage.driver !=null)
-        loginPage.driver.quit();
+        if (driver != null)
+            driver.quit();
         else System.out.println("There is no driver");
     }
-
-    public LoginPage getLoginPage() {
-        return loginPage;
-    }
-
 
 }
