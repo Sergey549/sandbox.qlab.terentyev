ff5ffbecbc840a830f2a4f29578c8b5b50fe418e
Sergey549 <55460819+Sergey549@users.noreply.github.com>
2020-05-16 15:24:06 +0300

    PageObject structure

--- src/test/java/login/LoginNegativeTests.java
+++ src/test/java/login/LoginNegativeTests.java
@@ -1,11 +1,6 @@
 package login;
-
-import org.junit.Assert;
 import org.junit.Test;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
+
 
 public class LoginNegativeTests extends TestBase {
 
@@ -15,7 +10,7 @@ public class LoginNegativeTests extends TestBase {
         String userName = "TestUser";
         String userPassword = "Password";
 
-        new ModernLoginPage()
+        new LoginPage()
                 .openLoginPage()
                 .fillUserNameField(userName)
                 .fillUserPasswordField(userPassword)
@@ -28,76 +23,10 @@ public class LoginNegativeTests extends TestBase {
 
     public void checkUrl() {
 
-        new ModernLoginPage()
+        new LoginPage()
                 .openLoginPage()
                 .enterButtonClick()
                 .catchAnExceptionMethod()
-                .checkCurrentUrlIsLoginPage();
+                .checkCurrentLoginPageUrlIsTrue();
     }
-
-
-
-    
-    public class ModernLoginPage {
-
-        @FindBy(name = "_username")
-        private WebElement userField;
-
-        @FindBy(id = "password")
-        private WebElement passwordField;
-
-        @FindBy(css = "input[value='Войти']")
-        private WebElement enterButton;
-
-        @FindBy(xpath = ".//*[text()='Invalid credentials.']/..")
-        private WebElement hiddenValidationWebElement;
-
-
-        public ModernLoginPage openLoginPage() {
-            driver.get("https://tt-develop.quality-lab.ru/login");
-            PageFactory.initElements(driver, this);
-            return this;
-        }
-
-        public ModernLoginPage fillUserNameField(String userName) {
-            userField.click();
-            userField.clear();
-            userField.sendKeys(userName);
-            return this;
-        }
-
-        public ModernLoginPage fillUserPasswordField(String userPassword) {
-            passwordField.click();
-            passwordField.clear();
-            passwordField.sendKeys(userPassword);
-            return this;
-        }
-
-        public ModernLoginPage catchAnExceptionMethod() {
-            try {
-                checkValidationWebElementIsPresent();
-            } catch (NoSuchElementException ignored) {
-                System.out.println("The entered userdata is correct. You will be logged in");
-            }
-            return this;
-        }
-
-        public ModernLoginPage enterButtonClick() {
-            enterButton.click();
-            return this;
-        }
-
-        public ModernLoginPage checkValidationWebElementIsPresent() {
-            hiddenValidationWebElement.isDisplayed();
-            return this;
-        }
-
-        public ModernLoginPage checkCurrentUrlIsLoginPage() {
-            driver.getCurrentUrl();
-            String URL = driver.getCurrentUrl();
-            Assert.assertEquals(URL, "https://tt-develop.quality-lab.ru/login");
-            return this;
-        }
-    }
-
 }
--- src/test/java/login/TestBase.java
+++ src/test/java/login/TestBase.java
@@ -1,17 +1,22 @@
 package login;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
-public class TestBase  {
-    public WebDriver driver;
+public class TestBase {
 
+    public WebDriver driver;
 
     @Before
     public void initWebDriver() {
@@ -37,6 +42,69 @@ public class TestBase  {
         if (driver != null)
             driver.quit();
         else System.out.println("There is no driver");
+    }
+
+    public class LoginPage {
+
+        @FindBy(name = "_username")
+        private WebElement userField;
+
+        @FindBy(id = "password")
+        private WebElement passwordField;
+
+        @FindBy(css = "input[value='Войти']")
+        private WebElement enterButton;
+
+        @FindBy(xpath = ".//*[text()='Invalid credentials.']/..")
+        private WebElement hiddenValidationWebElement;
+
+
+        public LoginPage openLoginPage() {
+            driver.get("https://tt-develop.quality-lab.ru/login");
+            PageFactory.initElements(driver, this);
+            return this;
+        }
+
+        public LoginPage fillUserNameField(String userName) {
+            userField.click();
+            userField.clear();
+            userField.sendKeys(userName);
+            return this;
+        }
+
+        public LoginPage fillUserPasswordField(String userPassword) {
+            passwordField.click();
+            passwordField.clear();
+            passwordField.sendKeys(userPassword);
+            return this;
+        }
+
+        public LoginPage catchAnExceptionMethod() {
+            try {
+                checkValidationWebElementIsPresent();
+            } catch (NoSuchElementException ignored) {
+                System.out.println("The entered userdata is correct. You will be logged in");
+            }
+            return this;
+        }
+
+        public LoginPage enterButtonClick() {
+            enterButton.click();
+            return this;
+        }
+
+        public LoginPage checkValidationWebElementIsPresent() {
+            hiddenValidationWebElement.isDisplayed();
+            return this;
+        }
+
+        public LoginPage checkCurrentLoginPageUrlIsTrue() {
+            driver.getCurrentUrl();
+            String URL = driver.getCurrentUrl();
+            Assert.assertEquals(URL, "https://tt-develop.quality-lab.ru/login");
+            return this;
+        }
+
     }
 
 }
