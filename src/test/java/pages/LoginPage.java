55fde405b7441eca7e99273b16d0036dd0adaba8
Sergey549 <55460819+Sergey549@users.noreply.github.com>
2020-05-17 20:09:57 +0300

    PageObject structure. Final

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
-    ;
+
     private final WebDriver driver;
 
     @FindBy(name = "_username")
